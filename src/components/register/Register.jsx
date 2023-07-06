b2e68c65aaa560b3883460801ca3b7eea8a886a3
Rodrigo Alba <rodrigoqalba@gmail.com>
2023-07-06 13:20:34 -0400

    created variables for skills and initialState in Register.jsx

---
+++ src/components/register/Register.jsx
@@ -0,0 +1,17 @@
+import "./register.css";
+
+const skills = [
+    { value: "react", label: "React" },
+    { value: "vuejs", label: "Vue.js" },
+    { value: "svelte", label: "Svelte" },
+    { value: "angular", label: "Angular" },
+    { value: "javascript", label: "JavaScript" },
+    { value: "nodejs", label: "Node.js" }
+];
+
+const initialState = {
+    name: "",
+    email: "",
+    password: ""
+};
+
