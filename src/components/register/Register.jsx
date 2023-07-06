82fe3283c28c0ad11e1ccfe9408e24a91e7a88a4
Rodrigo Alba <rodrigoqalba@gmail.com>
2023-07-06 13:52:07 -0400

    state, InputChange and FormSubmit

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./register.css";
 
 const skills = [
@@ -13,5 +14,44 @@ const initialState = {
     name: "",
     email: "",
     password: ""
+};
+
+const Register = () => {
+    const [state, setState] = useState(initialState);
+    const [subscribe, setSubscribe] = useState(false);
+    const [selectedSkill, setSelectedSkill] = useState(null);
+    const [ successMsg, setSuccessMsg] = useState("");
+    const [errorMsg, setErrorMsg] = useState("");
+
+    const handleInputChange = (event) => {
+        const { name, value } event.target;
+        setState((prevState) => {
+            return {
+                ...prevState,
+                [name]: value
+            };
+        });
+    };
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        setErrorMsg("");
+        setSuccessMSg("");
+        const allFieldsEntered = Object.keys(state).every(
+            (key) => state[key].trim() !== "" && selectedSkill && subscribe
+        );
+        if (allFieldsEntered) {
+            // code for doing registration
+            setSuccessMsg("You have successfully registered");
+            setTimeout(() => {
+                setSuccessMsg("");
+                setState(initialState);
+                setSelectedSkill(null);
+                setSubscribe(false);
+            }, 2000);
+        } else {
+            setErrorMsg("All the field are required.");
+        }
+    }
 };
 
